be96ec6ca116e3a3c9d99f43a47705386e772a67
rimek3 <rimek195@yandex.ru>
2018-10-15 20:44:06 +0300

    выполнил домашнее задание категории hard

---
+++ lesson1_homework_hard.py
@@ -0,0 +1,13 @@
+__author__ = 'Кузнецов Павел Дмитриевич'
+
+#Задача-1
+
+import math
+a = math.inf
+if a == a**2:
+    print(True)
+if a == a*2:
+    print(True)
+if a > 9999999:
+    print(True)
+
